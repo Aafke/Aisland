cbc3dc7dd7fc8631384b46fef4cf51ef6161b5d9
Meints <ame20844@BA92-2T81KKGTDY.local>
2017-06-30 14:16:24 +0200

    initial commit

---
+++ src/main/java/com/island/aafke/AislandApplication.java
@@ -0,0 +1,12 @@
+package com.island.aafke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AislandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AislandApplication.class, args);
+	}
+}
